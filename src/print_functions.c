39a93bc5fe02f1e314d057f8b98e98f8ce391506
ihuseyin <borzslam@gmail.com>
2025-02-23 18:57:12 +0300

    Add files via upload

--- src/print_functions.c
+++ src/print_functions.c
@@ -19,16 +19,19 @@ void	print_usage(void)
 	char	*text;
 
 	text = "Usage: ./fractol [FRACTAL SET]... [OPTIONS]...\n\n"
-		"  'cr' is the real value of constant\n"
-		"  'ci' is the imaginary value of constant\n"
-		"  'i' is the numbe of iterations\n\n"
-		"  Fractal sets with options:\n"
-		"\tmandelbrot  <i>\n"
-		"\tjulia <cr> <ci> <i>\n"
-		"\tburningship <i>\n\n"
-		"./fractol mandelbrot 1000\n"
-		"./fractol julia -0.8 0.156 500\n"
-		"./fractol burninigship 500\n";
+		"  Options:\n  <cr> - Real value of constant (for Julia set).\n"
+		"  <ci> - Imaginary value of constant (for Julia set).\n"
+		"  <i>  - Number of iterations\n\n"
+		"  Available Fractal Sets:\n\n"
+		"   Mandelbrot Set\n"
+		"\tSyntax: ./fractol mandelbrot <i>\n"
+		"\tExample: ./fractol mandelbrot 1000\n"
+		"   Julia Set\n"
+		"\tSyntax: ./fractol julia <cr> <ci> <i>\n"
+		"\tExample: ./fractol julia -0.8 0.156 500\n"
+		"   Burning Ship Set\n"
+		"\tSyntax: ./fractol burningship <i>\n"
+		"\tExample: ./fractol burningship 200\n";
 	print(text);
 	exit(EXIT_FAILURE);
 }
