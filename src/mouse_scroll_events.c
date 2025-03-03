3b042e2f1585c529eb9cc67aecc51b48823195ae
ihuseyin <borzslam@gmail.com>
2025-03-03 14:30:52 +0300

    Update mouse_scroll_events.c

--- src/mouse_scroll_events.c
+++ src/mouse_scroll_events.c
@@ -8,10 +8,10 @@ void	scroll_up(int x, int y, t_data *var)
 
 	mouse_x = map(var->map.min_x, var->map.max_x, x, WIN_W);
 	mouse_y = map(var->map.max_y, var->map.min_y, y, WIN_H);
-	var->map.min_x = mouse_x + (var->map.min_x - mouse_x) * SCALE_FACTOR;
-	var->map.max_x = mouse_x + (var->map.max_x - mouse_x) * SCALE_FACTOR;
-	var->map.min_y = mouse_y + (var->map.min_y - mouse_y) * SCALE_FACTOR;
-	var->map.max_y = mouse_y + (var->map.max_y - mouse_y) * SCALE_FACTOR;
+	var->map.min_x = mouse_x + (var->map.min_x - mouse_x) / SCALE_FACTOR;
+	var->map.max_x = mouse_x + (var->map.max_x - mouse_x) / SCALE_FACTOR;
+	var->map.min_y = mouse_y + (var->map.min_y - mouse_y) / SCALE_FACTOR;
+	var->map.max_y = mouse_y + (var->map.max_y - mouse_y) / SCALE_FACTOR;
 }
 
 void	scroll_down(int x, int y, t_data *var)
@@ -21,8 +21,8 @@ void	scroll_down(int x, int y, t_data *var)
 
 	mouse_x = map(var->map.min_x, var->map.max_x, x, WIN_W);
 	mouse_y = map(var->map.max_y, var->map.min_y, y, WIN_H);
-	var->map.min_x = mouse_x + (var->map.min_x - mouse_x) / SCALE_FACTOR;
-	var->map.max_x = mouse_x + (var->map.max_x - mouse_x) / SCALE_FACTOR;
-	var->map.min_y = mouse_y + (var->map.min_y - mouse_y) / SCALE_FACTOR;
-	var->map.max_y = mouse_y + (var->map.max_y - mouse_y) / SCALE_FACTOR;
+	var->map.min_x = mouse_x + (var->map.min_x - mouse_x) * SCALE_FACTOR;
+	var->map.max_x = mouse_x + (var->map.max_x - mouse_x) * SCALE_FACTOR;
+	var->map.min_y = mouse_y + (var->map.min_y - mouse_y) * SCALE_FACTOR;
+	var->map.max_y = mouse_y + (var->map.max_y - mouse_y) * SCALE_FACTOR;
 }
